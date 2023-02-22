c2026543c828b33446fae83c21c19e2fff8e69bd
Wallace <wallacesb@outlook.com.br>
2023-02-22 14:47:07 -0300

    Adicionando Metodo keyPress na Classe Menu

--- projeto01ContaBancaria/src/conta/Menu.java
+++ projeto01ContaBancaria/src/conta/Menu.java
@@ -1,8 +1,12 @@
 package conta;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 import conta.util.Cores;
 
 public class Menu {
@@ -11,7 +15,7 @@ public class Menu {
 		
 		Scanner leia = new Scanner(System.in);
 		
-		Conta c1 = new Conta(1, 123, 1, "Erica Araujo", 30000.0f);
+		/*Conta c1 = new Conta(1, 123, 1, "Erica Araujo", 30000.0f);
 		c1.visualizar();
 		
 		c1.setSaldo(35000.0f);
@@ -24,7 +28,19 @@ public class Menu {
 			System.out.println("\n\n" + c2.getSaldo());
 		
 		c1.depositar(10000.0f);
-		c1.visualizar();
+		c1.visualizar();*/
+		
+		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "Vitória", 30000.0f, 1000f);
+		cc1.visualizar();
+		cc1.sacar(30900);
+		cc1.visualizar();
+		
+		System.out.println();
+		
+		ContaPoupanca cp1 = new ContaPoupanca(1, 6548, 2, "Giovani", 30000.0f, 21);
+		cp1.visualizar();
+		cp1.sacar(29000);
+		cp1.visualizar();
 		
 		int opcao;
 		
@@ -46,13 +62,22 @@ public class Menu {
 			System.out.println("                     9 - Sair                                           ");
 			System.out.println("                                                                        ");
 			System.out.println("************************************************************************");		
+			System.out.println("                                                                        ");
 			System.out.println(Cores.TEXT_CYAN + "Entre com a opção desejada:                                             ");
 			System.out.println("                                                                        ");
 			
-			opcao = leia.nextInt();
+			try {
+				opcao = leia.nextInt();				
+			} catch (InputMismatchException e) {
+				System.out.println("\nDigite valores inteiros");
+				leia.nextLine();
+				opcao = 0;
+			}
+			
 			
 			if (opcao == 9) {
-				System.out.println(Cores.TEXT_BLUE_BOLD + "\n            Banco do Brazil com Z " + Cores.TEXT_RESET + Cores.ANSI_BLACK_BACKGROUND + Cores.TEXT_YELLOW + "- O seu Futuro começa aqui!           ");
+				System.out.println("\n                                                                        ");
+				System.out.println(Cores.TEXT_BLUE_BOLD + "            Banco do Brazil com Z " + Cores.TEXT_RESET + Cores.ANSI_BLACK_BACKGROUND + Cores.TEXT_YELLOW + "- O seu Futuro começa aqui!           ");
 				System.out.println("                                                                        ");
 				leia.close();
 				System.exit(0);
@@ -61,43 +86,53 @@ public class Menu {
 			switch (opcao) {
 			case 1:
 				System.out.println("Criar Conta                                                             \n\n");
-				
+				keyPress();
 				break;
 			case 2:
 				System.out.println("Listar todas as Contas                                                  \n\n");
-				
+				keyPress();
 				break;
 			case 3:
 				System.out.println("Consultar dados da Conta - por número                                   \n\n");
-				
+				keyPress();
 				break;
 			case 4:
 				System.out.println("Atualizar dados da Conta                                                \n\n");
-				
+				keyPress();
 				break;
 			case 5:
 				System.out.println("Apagar a Conta                                                          \n\n");
-				
+				keyPress();
 				break;
 			case 6:
 				System.out.println("Saque                                                                   \n\n");
-				
+				keyPress();
 				break;
 			case 7:
 				System.out.println("Depósito                                                                \n\n");
-				
+				keyPress();
 				break;
 			case 8:
 				System.out.println("Transferência entre contas                                              \n\n");
-				
+				keyPress();
 				break;
 			default:
 				System.out.println("\nOpção Inválida!                                                         \n");
+				keyPress();
 				break;
 			}
 			
 		}
 	
+	}
+	
+	public static void keyPress() {
+		try {
+			System.out.println("\n\nPressione " + Cores.TEXT_WHITE + "Enter" + Cores.TEXT_CYAN + " para continuar...                                       ");
+			System.in.read();
+		} catch (IOException e) {
+			System.out.println("Você pressionou uma tecla diferente de Enter                            ");		
+		}
 	}
 
 }
